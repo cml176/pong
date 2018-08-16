fe5bba05f3f0f67576ebba07859ce22bee605b50
Charles Llewellyn <cml176@txstate.edu>
2018-08-16 01:16:18 -0500

    formatting

--- Pong/test.cpp
+++ Pong/test.cpp
@@ -6,15 +6,16 @@ using namespace pongModel;
 
 
 int main() {
+	//set up pong model
 	const int width = 300;
 	const int height = 300;
 	Ball ball = Ball::Ball(width/2, height/2, 1,0,5);
 	Paddle leftPaddle = Paddle::Paddle(10, height/2, 25, 50);
 	Paddle rightPaddle = Paddle::Paddle(width-10, height/2, 25, 50);
 	PongTable table = PongTable(leftPaddle, rightPaddle, ball, width, height);
-
-
-
-
+	//set up pong view
+		//TODO:
+	//set up pong controller 
+		//TODO:
 	return 0;
 }
