937fcc666904cee2555597d82eebedd27a605e74
cml176 <cml176@txstate.edu>
2018-08-23 13:03:05 -0500

    syntax fix on PongView

--- Pong/view/PongView.h
+++ Pong/view/PongView.h
@@ -1,14 +1,17 @@
+#include "../model/model_headers/PongTable.h"
+
 #ifndef PongView_H
 #define PongView_H
+class pongModel::PongTable;
 
 class PongView{
 	pongModel::PongTable model;
 	
 	public:
-		PongView(const pongModel::PongTable::PongTable &model);
+		PongView(const pongModel::PongTable &model);
 		
 		void initGLUT();
 		
-		void SetModel(const pongModel::PongTable::PongTable &model);
-}
+		void SetModel(const pongModel::PongTable &model);
+};
 #endif
